1ba9131238192b74b88d6c4308b6d3e09abcf16d
Jared Tobin <jared@jtobin.ca>
2015-10-10 00:38:41 +1300

    Fix test (whoops).

--- test/Rosenbrock.hs
+++ test/Rosenbrock.hs
@@ -3,7 +3,6 @@
 module Main where
 
 import Numeric.MCMC
-import Data.Sampling.Types
 
 target :: [Double] -> Double
 target [x0, x1] = negate (5  *(x1 - x0 ^ 2) ^ 2 + 0.05 * (1 - x0) ^ 2)
@@ -18,5 +17,5 @@ transition =
     (sampleT (slice 2.0) (slice 3.0))
 
 main :: IO ()
-main = withSystemRandom . asGenIO $ mcmc 10000 [0, 0] rosenbrock transition
+main = withSystemRandom . asGenIO $ mcmc 10000 [0, 0] transition rosenbrock
 
